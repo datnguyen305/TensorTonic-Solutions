4061c862f55ef8f54368eef2922ee3dd9fe4c70b
dat_nguyen <159440406+datnguyen305@users.noreply.github.com>
2026-02-06 11:46:14 +0700

    Update solution for dropout-training

--- dropout-training/dropout-training.py
+++ dropout-training/dropout-training.py
@@ -5,18 +5,13 @@ def dropout(x, p=0.5, rng=None):
     Apply dropout to input x with probability p.
     Return (output, dropout_pattern).
     """
-    if rng is None:
+    if rng is None: 
         rng = np.random
+    
+    test = rng.random(x.shape)
 
-    q = 1 - p
+    pattern = np.where(test < (1-p), 1/(1-p), 0)
 
-    # random values in [0, 1)
-    r = rng.random(x.shape)
-
-    # pattern: 0 (drop) or 1/q (keep + scale)
-    pattern = np.where(r < q, 1 / q, 0)
-
-    # apply pattern
     output = x * pattern
 
     return output, pattern
